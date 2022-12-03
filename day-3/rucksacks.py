ee217dc82e61408512fcc4bf8834633b3ffc5d3d
Jacques Andre <jacquesandre@mailbox.org>
2022-12-03 18:40:37 +0000

    [feature] added part_2 to day-3

--- day-3/rucksacks.py
+++ day-3/rucksacks.py
@@ -31,11 +31,53 @@ def part_1() -> int:
 
             total += letter_value
 
-            print(f"common_letter:{common_letter}, value:{letter_value}")
     return total
+
+
+def part_2() -> int:
+    total = 0
+    all_lines = []
+    with open("input.txt") as file:
+        all_lines = [x.strip() for x in file]
+
+    for i in range(0, len(all_lines),6):
+        # TODO: refactor, terrible (but works)
+        group_1_rucksack_1 = (list(all_lines[i]))
+        group_1_rucksack_2 = (list(all_lines[i+1]))
+        group_1_rucksack_3 = (list(all_lines[i+2]))
+
+        group_2_rucksack_1 = (list(all_lines[i+3]))
+        group_2_rucksack_2 = (list(all_lines[i+4]))
+        group_2_rucksack_3 = (list(all_lines[i+5]))
+
+        common_letter_group_1 = list(set(group_1_rucksack_1).intersection(group_1_rucksack_2, group_1_rucksack_3))[0]
+        common_letter_group_2 = list(set(group_2_rucksack_1).intersection(group_2_rucksack_2, group_2_rucksack_3))[0]
+
+
+        letter_value = 0
+        if common_letter_group_1 in alphabet_lower:
+            letter_value = alphabet_lower.index(common_letter_group_1) + 1
+            total += letter_value
+
+        if common_letter_group_1 in alphabet_upper:
+            letter_value = alphabet_upper.index(common_letter_group_1) + 27
+            total += letter_value
+
+        if common_letter_group_2 in alphabet_lower:
+            letter_value = alphabet_lower.index(common_letter_group_2) + 1
+            total += letter_value
+
+        if common_letter_group_2 in alphabet_upper:
+            letter_value = alphabet_upper.index(common_letter_group_2) + 27
+            total += letter_value
+
+    return total
+
 
 
 
 if __name__ == "__main__":
     part1_ans = part_1()
-    print(part1_ans)
+    print(f"part_1: {part1_ans}")
+    part2_ans = part_2()
+    print(f"part_2: {part2_ans}")
