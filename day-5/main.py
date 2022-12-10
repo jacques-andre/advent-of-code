b7632c77f337d4b5cd5b5a6f67aa9779ba7d6277
Jacques Andre <jacquesandre@mailbox.org>
2022-12-10 14:42:36 +0000

    [feature] updated day-5

--- day-5/main.py
+++ day-5/main.py
@@ -11,6 +11,12 @@ stacks = {
         9: ['P', 'G', 'S'],
         }
 
+# example_stacks = {
+#         1: ["Z","N"],
+#         2: ["M","C","D"],
+#         3: ["P"]
+#         }
+
 def part_1() -> str:
     with open('input-new.txt') as file:
         lines = [x.strip() for x in file]
@@ -32,7 +38,37 @@ def part_1() -> str:
         print(f"{key}:{value}")
     return ans
 
+def part_2() -> str:
+    with open('input-new.txt') as file:
+        lines = [x.strip() for x in file]
+
+    for line in lines:
+        command = line.split(' ')
+
+        amount_to_move = int(command[1])
+        from_stack = int(command[3])
+        to_stack = int(command[5])
+
+        if amount_to_move == 1:
+            value_pop = stacks[from_stack].pop()
+            stacks[to_stack].append(value_pop)
+        else:
+            to_move = stacks[from_stack][-amount_to_move:]
+            del stacks[from_stack][-amount_to_move:]
+            stacks[from_stack][:amount_to_move]
+            stacks[to_stack].extend(to_move)
+                
+
+    ans = ""
+    for key, value in stacks.items():
+        ans += value[-1]
+        print(f"{key}:{value}")
+    return ans
+
 
 if __name__ == "__main__":
     part_1_ans = part_1()
     print(f"part_1_ans:{part_1_ans}")
+    part_2_ans = part_2()
+    print(f"part_2_ans:{part_2_ans}")
+    print(part_2_ans[::-1])
